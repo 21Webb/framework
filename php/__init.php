1d37a09bbff0241e915383a7d056a7c61072cef7
Brent Meeusen <32615059+Brent21Webb@users.noreply.github.com>
2019-02-14 13:47:43 +0100

    Ready for testing

--- php/__init.php
+++ php/__init.php
@@ -1,8 +1,13 @@
 <?php
 
-
-
-
+function _INIT($authorisation, $authorisationResult, $message, $location) {
+	global $AUTH; global $AUTHRES; global $MSG; global $HEAD;
+	$AUTH = $authorisation;
+	$AUTHRES = $authorisationResult;
+	$MSG = $message;
+	$HEAD = $location;
+}
 
 ?>
+
 
